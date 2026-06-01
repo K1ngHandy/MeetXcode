71097effa31c90dc4e534afceefbe4aba47d1471
Stephen Handy <K1ngHandy@users.noreply.github.com>
2026-06-01 18:24:47 -0400

    add project resources

---
+++ MeetXcode/MeetXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetXcodeApp.swift
+//  MeetXcode
+//
+//  Created by Steve Handy on 2026.06.01.
+//
+
+import SwiftUI
+
+@main
+struct MeetXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
